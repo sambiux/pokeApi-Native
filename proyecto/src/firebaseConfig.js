41468847d1fb193d38959d8b0e58080d05bad465
sambiux <sddaza.9@gmail.com>
2025-11-14 22:11:48 +0000

    Agregando la configuracio inicial para la conexion de la base de datos

--- proyecto/src/firebaseConfig.js
+++ proyecto/src/firebaseConfig.js
@@ -2,12 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-apiKey: "cambiar este dato",
-authDomain: "cambiar este dato",
-projectId: "cambiar este dato",
-storageBucket: "cambiar este dato",
-messagingSenderId: "cambiar este dato",
-appId: "cambiar este dato"
+  apiKey: "AIzaSyAIIdh6F_L_vpgLPrSbhg1i2IP1L64bBV0",
+  authDomain: "pokeapi-18cea.firebaseapp.com",
+  projectId: "pokeapi-18cea",
+  storageBucket: "pokeapi-18cea.firebasestorage.app",
+  messagingSenderId: "54323959480",
+  appId: "1:54323959480:web:669787b0e725598543064d",
+  measurementId: "G-HSGM8MJGT0"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
